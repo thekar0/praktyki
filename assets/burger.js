3dd637cd0e4bbe2aa929f87fc1bf4fba87f495f8
kar0 <okar2007ka@gmail.com>
2025-08-25 11:03:27 +0200

    burger fixed

--- assets/burger.js
+++ assets/burger.js
@@ -5,8 +5,16 @@ const burger = document.getElementById("burger")
 const burger_elements = Array.from(document.getElementsByClassName("burger_element"))
 const navbar = document.getElementsByClassName("navbar")[0]
 const burger_holder = document.getElementById("burger_holder")
-
+const dropdowns = Array.from(document.getElementsByClassName("dropdown"))
 let unrolled = false
+
+
+    dropdowns.forEach(dropdown => {
+        dropdown.addEventListener('click', function()
+    {
+            burger_holder.style.overflow = "visible"
+    })
+    });
 
 burger.addEventListener('click', function()
 {
@@ -14,14 +22,12 @@ burger.addEventListener('click', function()
 
     if(!unrolled)
     {
-        burger_holder.style.maxHeight = "100rem"
+        unrolled = true
+        burger_holder.style.maxHeight = "10rem"
         burger_holder.style.padding = "5%"
-        burger_holder.style.overflow = "visible"
         burger_elements.forEach(element => {
             element.style.filter = "brightness(0.5)"
         });
-        
-        unrolled = true
     }
     else
     {
@@ -34,6 +40,7 @@ burger.addEventListener('click', function()
 
         unrolled = false
     }
+
 })
 
 })
